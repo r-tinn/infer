26835bee007b7ccf29683c8ed91f7bd5c9b55120
Robert Tinn <t-rotinn@microsoft.com>
2019-08-09 17:26:35 +0100

    Run modified tutorial

--- src/Tutorials/GaussianProcessClassifier.cs
+++ src/Tutorials/GaussianProcessClassifier.cs
@@ -15,6 +15,7 @@ namespace Microsoft.ML.Probabilistic.Tutorials
     {
         public void Run()
         {
+            // engine has expectation propagation as an algorithm
             InferenceEngine engine = new InferenceEngine();
             if (!(engine.Algorithm is Algorithms.ExpectationPropagation))
             {
@@ -23,56 +24,88 @@ namespace Microsoft.ML.Probabilistic.Tutorials
             }
     
             // The data
+            //Vector[] inputs = new Vector[]
+            //{
+            //    Vector.FromArray(new double[2] { 0, 0 }),
+            //    Vector.FromArray(new double[2] { 0, 1 }),
+            //    Vector.FromArray(new double[2] { 1, 0 }),
+            //    Vector.FromArray(new double[2] { 0, 0.5 }),
+            //    Vector.FromArray(new double[2] { 1.5, 0 }),
+            //    Vector.FromArray(new double[2] { 0.5, 1.0 })
+            //};
+
             Vector[] inputs = new Vector[]
             {
-                Vector.FromArray(new double[2] { 0, 0 }),
-                Vector.FromArray(new double[2] { 0, 1 }),
-                Vector.FromArray(new double[2] { 1, 0 }),
-                Vector.FromArray(new double[2] { 0, 0.5 }),
-                Vector.FromArray(new double[2] { 1.5, 0 }),
-                Vector.FromArray(new double[2] { 0.5, 1.0 })
+                Vector.FromArray(new double[1] { 1 }),
+                Vector.FromArray(new double[1] { 2 }),
+                Vector.FromArray(new double[1] { 3 }),
+                Vector.FromArray(new double[1] { 3 }),
+                Vector.FromArray(new double[1] { 3 }),
+                Vector.FromArray(new double[1] { 4 }),
+                Vector.FromArray(new double[1] { 4,}),
+                Vector.FromArray(new double[1] { 4 }),
+                Vector.FromArray(new double[1] { 5 }),
+                Vector.FromArray(new double[1] { 5 }),
             };
-            
-            bool[] outputs = { true, true, false, true, false, false };
 
-            // Open an evidence block to allow model scoring
-            Variable<bool> evidence = Variable.Bernoulli(0.5).Named("evidence");
+            //bool[] outputs = { true, true, false, true, false, false };
+            //double[] outputs = { 0.7, 0.8, 0.2, 0.6, 0.4, 0.3 };
+            //double[] outputs = { 0.1, 6.6, 4.4, 13.2, 39.9, 11.8, 38.1, 12.6, 20.9, 40.3 };
+            double[] outputs = { 0.7, 0.8, 0.2, 0.6, 0.4, 0.3, 0.2, 0.6, 0.4, 0.3 };
+
+            // Open an evidence block to allow model scoring.
+            // All modelling code must be inside the IF block.
+            Variable<bool> evidence = Variable.Bernoulli(0.5).Named("evidence"); // marginal likelihood
             IfBlock block = Variable.If(evidence);
 
+            // Add constraints to evidence - 7
             // Set up the GP prior, which will be filled in later
+            // named just names the variable whatever its called
             Variable<SparseGP> prior = Variable.New<SparseGP>().Named("prior");
-
             // The sparse GP variable - a distribution over functions
             Variable<IFunction> f = Variable<IFunction>.Random(prior).Named("f");
-
+ 
             // The locations to evaluate the function
             VariableArray<Vector> x = Variable.Observed(inputs).Named("x");
             Range j = x.Range.Named("j");
 
             // The observation model
-            VariableArray<bool> y = Variable.Observed(outputs, j).Named("y");
+            VariableArray<double> y = Variable.Observed(outputs, j).Named("y");
             Variable<double> score = Variable.FunctionEvaluate(f, x[j]);
-            y[j] = (Variable.GaussianFromMeanAndVariance(score, 0.1) > 0);
+            VariableArray<double> noisyScore = Variable.Observed(outputs, j).Named("noisyScore");
+            using (Variable.ForEach(j))
+            {
+                var precision = Variable.GammaFromShapeAndRate(10, 2).Named("precision");
+                noisyScore[j] = Variable.GaussianFromMeanAndPrecision(score, precision);
+            }
+            y[j] = Variable.GaussianFromMeanAndVariance(noisyScore[j], 0.1);
 
             // Close the evidence block
             block.CloseBlock();
 
             // The basis
+            //Vector[] basis = new Vector[]
+            //{
+            //    Vector.FromArray(new double[2] { 0.2, 0.2 }),
+            //    Vector.FromArray(new double[2] { 0.2, 0.8 }),
+            //    Vector.FromArray(new double[2] { 0.8, 0.2 }),
+            //    Vector.FromArray(new double[2] { 0.8, 0.8 })
+            //};
             Vector[] basis = new Vector[]
             {
-                Vector.FromArray(new double[2] { 0.2, 0.2 }),
-                Vector.FromArray(new double[2] { 0.2, 0.8 }),
-                Vector.FromArray(new double[2] { 0.8, 0.2 }),
-                Vector.FromArray(new double[2] { 0.8, 0.8 })
-            };
+                Vector.FromArray(new double[1] { 0.2 }),
+                Vector.FromArray(new double[1] { 0.2 }),
+                Vector.FromArray(new double[1] { 0.8 }),
+                Vector.FromArray(new double[1] { 0.8 })
+               };
 
-            for (int trial = 0; trial < 3; trial++)
+            for (int trial = 0; trial < 1; trial++)
             {
                 // The kernel
                 IKernelFunction kf;
                 if (trial == 0)
                 {
-                    kf = new SquaredExponential(-0.0);
+                    kf = new SquaredExponential(-0.5);
                 }
                 else if (trial == 1)
                 {
@@ -88,6 +121,8 @@ namespace Microsoft.ML.Probabilistic.Tutorials
                 prior.ObservedValue = new SparseGP(new SparseGPFixed(gp, basis));
 
                 // Model score
+                // compile the inference algorithm
+                // execute
                 double NNscore = engine.Infer<Bernoulli>(evidence).LogOdds;
                 Console.WriteLine("{0} evidence = {1}", kf, NNscore.ToString("g4"));
             }
@@ -102,8 +137,8 @@ namespace Microsoft.ML.Probabilistic.Tutorials
             {
                 Gaussian post = sgp.Marginal(inputs[i]);
                 double postMean = post.GetMean();
-                string comment = (outputs[i] == (postMean > 0.0)) ? "correct" : "incorrect";
-                Console.WriteLine("f({0}) = {1} ({2})", inputs[i], post, comment);
+                //string comment = (realOutputs[i] - postMean == (postMean > 0.0)) ? "correct" : "incorrect";
+                Console.WriteLine("f({0}) = {1}", inputs[i], post);
             }
         }
     }
--- src/Tutorials/RunMe.cs
+++ src/Tutorials/RunMe.cs
@@ -35,7 +35,7 @@ namespace Microsoft.ML.Probabilistic.Tutorials
             //Tutorials
             //Uncomment one of these lines to run a particular tutorial in console application
 
-            new FirstExample().Run();
+            //new FirstExample().Run();
             //new TruncatedGaussian().Run();
             //new TruncatedGaussianEfficient().Run();
             //new LearningAGaussian().Run();
@@ -56,7 +56,7 @@ namespace Microsoft.ML.Probabilistic.Tutorials
             //new ChessAnalysis().Run();
             //new ClickModel().Run();
             //new DifficultyAbility().Run();
-            //new GaussianProcessClassifier().Run();
+            new GaussianProcessClassifier().Run();
             //new MultinomialRegression().Run();
             //new RecommenderSystem().Run();
             //new StudentSkills().Run();
